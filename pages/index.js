54740218f46c6a3afb9d7bb4667dd91e1b7ac334
Rob Hess <robwhess@gmail.com>
2019-12-09 14:18:36 -0800

    Get site initially set up.

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+function Index() {
+  return (
+    <Layout>
+      <h1>Hello!</h1>
+    </Layout>
+  );
+}
+
+export default Index;

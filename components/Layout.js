1e67098efd3ef5502b73e4f52f1656ac9e185394
Rob Hess <robwhess@gmail.com>
2019-12-10 19:47:15 -0800

    Add basic Emotion styling.

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Global, css } from '@emotion/core';
+
+const globalStyles = css`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 300;
+  }
+`;
 
 function Layout(props) {
   return (
     <div>
+      <Global styles={globalStyles} />
       {props.children}
     </div>
   );
